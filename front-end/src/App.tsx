4badc7d8323884778402d5c2fa3de2d8eac81a74
itsanji <nhu.nhu.face@gmail.com>
2023-08-25 01:13:16 +0900

    int front-end

---
+++ front-end/src/App.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function App() {
+    return <main>{/* <TodoApp /> */}</main>;
+}
+
+export default App;
---
+++ front-end/src/types.d.ts
@@ -0,0 +1,10 @@
+interface User {
+    username: string;
+    id: number;
+}
+
+interface Todo {
+    content: string;
+    id: number;
+    status: boolean;
+}
